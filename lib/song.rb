41c05c8a790acff41978b315fc79ca9c60424524
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-15 06:46:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   def self.new_by_filename(name)
     split_name = name.split(" - ")
     song = self.new(split_name[1])
-    song.artist.add_song(song)
+    song.artist = split_name[0]
   end
 
 end
